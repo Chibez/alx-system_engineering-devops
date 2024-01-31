50564d29d0cb63c665dae7c16dd03e0545997a42
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2024-01-31 15:41:12 -0800

    Simply matching School

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
-# This script uses a regular expression to find and print 
-# occurrences of the exact string "School" in a case-sensitive manner.
+# This script uses a regular expression to find and print
+# occurrences of the exact string "School."
+#
 puts ARGV[0].scan(/School/).join
