ffffdd8a9910141b02b97cbd75d4ceedcb8ae465
caleb odobi <odocaleb@gmail.com>
2024-05-07 06:28:15 -0700

    query Api and return the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,6 @@ def number_of_subscribers(subreddit):
     function that queries the Reddit API and returns the number of subscribers
     (not active users, total subscribers) for a given subreddit.
     """
-
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
