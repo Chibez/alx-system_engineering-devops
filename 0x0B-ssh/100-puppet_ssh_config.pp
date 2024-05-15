0f50dd3ff0b0fe51f04303597fc3f2cc108cceed
caleb odobi <odocaleb@gmail.com>
2024-05-15 02:54:56 -0700

    Redu of ssh project

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,22 +1,13 @@
 #!/usr/bin/env bash
-file { 'ect/ssh/ssh_config':
-  ensure  => present,
-  owner   => 'ubuntu',
-  group   => 'ubuntu', 
-  mode    => '0600',
-  content => "\
-# SSH client configuration
 
-# Default settings
-Host *
-    # Disable password authentication
-    PasswordAuthentication no
+file { 'ect/ssh/ssh_cofig':
+	ensure  => present,
+  
+  content => "
 
-# Configuration for the server you want to connect to
-Host 524063-web-01
-    # Specify the private key file
-    IdentityFile ~/.ssh/school
-    HostName 54.165.109.213
-    User ubuntu
-",
+	#SSH client configuration
+	Host*
+	IdentityFile ~/.ssh/school
+	passwordAuthentication no
+	",
 }
